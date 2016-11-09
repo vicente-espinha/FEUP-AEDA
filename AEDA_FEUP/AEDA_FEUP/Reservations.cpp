bc14a619fad092e82694f85f3b13b9247b7e269f
Vicente Espinha <up201503764@fe.up.pt>
2016-11-09 11:22:17 +0000

    reservations in the house :100:

---
+++ AEDA_FEUP/AEDA_FEUP/Reservations.cpp
@@ -0,0 +1,12 @@
+#include "Reservations.h"
+/*
+Reservation(string name, string type, string type_type, double price, Date d1, Date d2)
+{
+	this->name = name;
+	this->type = type;
+	this->type_type = type_type;
+	this->total_price = price;
+	this->d1 = d1;
+	this->d2 = d2;
+}
+*/
---
+++ AEDA_FEUP/AEDA_FEUP/Reservations.h
@@ -0,0 +1,14 @@
+#include "Date.h"
+
+
+class Reservation {
+private:
+	string name, type, type_type;
+	double total_price;
+	Date d1, d2;
+public:
+	Reservation(string name, string type, string type_type, double price, Date d1, Date d2);
+
+
+
+};
