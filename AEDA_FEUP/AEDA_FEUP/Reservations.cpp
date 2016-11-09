b346ebc9b5ddda9b9759f545c8e96f913c1856cf
Vicente Espinha <up201503764@fe.up.pt>
2016-11-09 12:54:58 +0000

    Reservations in the house 2 :100:

--- AEDA_FEUP/AEDA_FEUP/Reservations.cpp
+++ AEDA_FEUP/AEDA_FEUP/Reservations.cpp
@@ -1,12 +1,13 @@
 #include "Reservations.h"
-/*
-Reservation(string name, string type, string type_type, double price, Date d1, Date d2)
+template<typename T>
+Reservation::Reservation(string name, string type, string type_type, int n,double price, Date d1, Date d2,Rent<T> * rent)
 {
 	this->name = name;
-	this->type = type;
-	this->type_type = type_type;
+	this->type_rent = type;
+	this->type_type_rent = type_type;
 	this->total_price = price;
 	this->d1 = d1;
 	this->d2 = d2;
+	this->n_people=n;
+	this->rent = rent;
 }
-*/
--- AEDA_FEUP/AEDA_FEUP/Reservations.h
+++ AEDA_FEUP/AEDA_FEUP/Reservations.h
@@ -1,14 +1,23 @@
 #include "Date.h"
+#include "Rent.h"
 
-
+using namespace std;
+template <typename T>
 class Reservation {
 private:
-	string name, type, type_type;
-	double total_price;
 	Date d1, d2;
+	double total_price;
+	int n_people;
+	string name, type_rent,type_type_rent;
+	Rent<T> *rent;
 public:
-	Reservation(string name, string type, string type_type, double price, Date d1, Date d2);
-
-
-
+	Reservation(string name, string type, string type_type, int n,double price, Date d1, Date d2,Rent<T> * rent);
+	string getname() { return name; };
+	string gettype_rent() { return type_rent; };
+	string gettype_type_rent() { return type_type_rent; };
+	double getprice() { return total_price; };
+	Date getDate1() {return d1;};
+	Date getDate2() { return d2;};
+	int getn_people() { return n_people; };
+	Rent<T> getrent() { return rent; };
 };
